f0e23c99018e3ebad469c908b22eafc0ded8fbf0
Ali Bilgin Arslan <aarslan@Ford-Prefect.local>
2011-06-05 13:59:17 -0400

    balh

--- demo/hmax_cvpr06_run_cal101.m
+++ demo/hmax_cvpr06_run_cal101.m
@@ -113,7 +113,7 @@ fprintf('CREATING S2 FEATURE DICTIONARY BY SAMPLING FROM TRAINING IMAGES\n');
 lib = struct;
 
 m = hmax.Model(p, lib);
-cns('init', m, 'gpu2');
+cns('init', m);
 
 count = min(numel(trainPaths), numFeatures);
 
